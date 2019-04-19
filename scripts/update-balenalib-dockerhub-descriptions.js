392f3a4fe1bfe96b0c8ae6c510107a27942bf99b
Trong Nghia Nguyen <nghiant2710@gmail.com>
2019-04-19 21:28:40 +0700

    scripts: Update script to get description from base images repo
    Change-type: patch
    Signed-off-by: Trong Nghia Nguyen <nghiant2710@gmail.com>

--- scripts/update-balenalib-dockerhub-descriptions.js
+++ scripts/update-balenalib-dockerhub-descriptions.js
@@ -22,51 +22,16 @@ const handlebars = require('handlebars')
 const path = require('path')
 const utils = require('./utils')
 
-const TEMPLATE_PATHS = {
-  'arch': {
-    'short': path.join(__dirname, '../template-helpers/balena-arch-os-short-description.tpl'),
-    'full': path.join(__dirname, '../template-helpers/balena-arch-os-full-description.tpl')
-  },
-  'device': {
-    'short': path.join(__dirname, '../template-helpers/balena-device-os-short-description.tpl'),
-    'full': path.join(__dirname, '../template-helpers/balena-device-os-full-description.tpl')
-  },
-  'stack': {
-    'short': path.join(__dirname, '../template-helpers/balena-stack-short-description.tpl'),
-    'full': path.join(__dirname, '../template-helpers/balena-stack-full-description.tpl')
-  }
-}
-
 const BALLENA_LIB_GH_URL = 'https://github.com/balena-io-library/base-images'
-const DEVICE_LIST = ['raspberry-pi2', 'qemux86-64', 'beaglebone-black', 'intel-nuc', 'via-vab820-quad', 'zynq-xz702', 'odroid-c1', 'odroid-xu4', 'parallella', 'nitrogen6x', 'hummingboard', 'ts4900', 'colibri-imx6dl', 'apalis-imx6q', 'raspberrypi3', 'artik5', 'artik10', 'beaglebone-green-wifi', 'beaglebone-green', 'artik710', 'am571x-evm', 'up-board', 'kitra710', 'imx6ul-var-dart', 'kitra520', 'jetson-tx2', 'jetson-tx1', 'generic-aarch64', 'generic-armv7ahf', 'bananapi-m1-plus', 'orangepi-plus2', 'fincm3', 'artik533s', 'artik530', 'orbitty-tx2', 'spacely-tx2', 'revpi-core-3', 'asus-tinker-board-s', 'asus-tinker-board', 'var-som-mx6', 'nanopi-neo-air', 'imx7-var-som', 'beaglebone-pocket', 'intel-edison', 'qemux86', 'stem-x86-32', 'cybertan-ze250', 'iot2000', 'ts7700', 'raspberry-pi', 'imx8m-var-dart', 'cl-som-imx8']
+const DOCKERHUB_DESC_URL = 'https://raw.githubusercontent.com/balena-io-library/base-images/master/balena-base-images/docs/dockerhub/'
+const SHORT_DESCRIPTION = 'This image is part of the balena.io base image series for IoT devices.'
+
+//const DEVICE_LIST = ['raspberry-pi2', 'qemux86-64', 'beaglebone-black', 'intel-nuc', 'via-vab820-quad', 'zynq-xz702', 'odroid-c1', 'odroid-xu4', 'parallella', 'nitrogen6x', 'hummingboard', 'ts4900', 'colibri-imx6dl', 'apalis-imx6q', 'raspberrypi3', 'artik5', 'artik10', 'beaglebone-green-wifi', 'beaglebone-green', 'artik710', 'am571x-evm', 'up-board', 'kitra710', 'imx6ul-var-dart', 'kitra520', 'jetson-tx2', 'jetson-tx1', 'generic-aarch64', 'generic-armv7ahf', 'bananapi-m1-plus', 'orangepi-plus2', 'fincm3', 'artik533s', 'artik530', 'orbitty-tx2', 'spacely-tx2', 'revpi-core-3', 'asus-tinker-board-s', 'asus-tinker-board', 'var-som-mx6', 'nanopi-neo-air', 'imx7-var-som', 'beaglebone-pocket', 'intel-edison', 'qemux86', 'stem-x86-32', 'cybertan-ze250', 'iot2000', 'ts7700', 'raspberry-pi', 'imx8m-var-dart', 'cl-som-imx8']
+const DEVICE_LIST = ['imx8m-var-dart', 'cl-som-imx8']
 const ARCH_LIST = ['armv7hf', 'i386', 'amd64', 'aarch64', 'rpi', 'armv5e', 'i386-nlp']
 const STACK_LIST = ['node', 'python', 'golang', 'openjdk']
 
-function getDescriptionPath (repoInfo) {
-  if (STACK_LIST.some(s => repoInfo.imageName.includes(s))) {
-    // imageName contains a stack => return path to templates for stack
-    return TEMPLATE_PATHS.stack
-  }
-  if (ARCH_LIST.some(s => repoInfo.imageName.includes(s))) {
-    // imageName contains a stack => return path to templates for stack
-    return TEMPLATE_PATHS.arch
-  }
-  return TEMPLATE_PATHS.device
-}
-
-function generateRepoDescription (repoInfo) {
-  const templatePaths = getDescriptionPath(repoInfo)
-  const descriptions = {}
-
-  for (const type of Object.keys(templatePaths)) {
-    var template = handlebars.compile(fs.readFileSync(templatePaths[type], 'utf8'))
-    descriptions[type] = template(repoInfo)
-  }
-  return descriptions
-}
-
 var devices = DEVICE_LIST.concat(ARCH_LIST)
-
 var imageNames = ['-debian', '-debian-node', '-debian-python', '-debian-golang', '-debian-openjdk']
 imageNames = imageNames.concat(['-alpine', '-alpine-node', '-alpine-python', '-alpine-golang', '-alpine-openjdk'])
 imageNames = imageNames.concat(['-fedora', '-fedora-node', '-fedora-python', '-fedora-golang', '-fedora-openjdk'])
@@ -91,16 +56,19 @@ dhAPI.login(dhAccount, dhPassword)
           'imageName': device + (imageName ? imageName : '-debian')
         }
 
-        repoInfo['tagList'] = utils.getAllRepoTags(repoInfo.imageName)
-
-        if (!repoInfo['tagList']) {
-          console.log(repoInfo.imageName + ' failed!')
+        var rawContent = execSync(`curl -sSL --compressed ${DOCKERHUB_DESC_URL + repoInfo.imageName}-full-description.md`).toString()
+        if (!rawContent.includes('404: Not Found')) {
+        } else {
+          console.log(`Missing descriptions for ${repoInfo.imageName}!`)
           continue
         }
 
-        repoInfo['descriptions'] = generateRepoDescription(repoInfo)
-        utils.setDockerhubDescription(info.token, repoInfo)
+        repoInfo['descriptions'] = {
+          'short': SHORT_DESCRIPTION,
+          'full': rawContent
+        }
 
+        utils.setDockerhubDescription(info.token, repoInfo)
         if (repoInfo.stackName.includes('debian')) {
           repoInfo.imageName = repoInfo.imageName.replace(/-debian/, '')
           utils.setDockerhubDescription(info.token, repoInfo)
--- scripts/utils.js
+++ scripts/utils.js
@@ -19,8 +19,6 @@
 const dhAPI = require('docker-hub-api')
 const { execSync } = require('child_process');
 
-const OFFICIAL_IMAGES_REPO_URL = 'https://raw.githubusercontent.com/balena-io-library/official-images/master/library/'
-
 /**
  * @module utils
  */
@@ -49,45 +47,4 @@ exports.setDockerhubDescription = (token, repoInfo) => {
   .catch ((error) => {
     console.log(`Can't set ${repoInfo.imageName}!`)
   })
-}
-
-/**
- * @summary Download and extract tags for dockerhub full description from library file from balena-io-library/official-image repo
- * @function
- * @memberof module:utils
- * @public
- *
- * @param {String} repo - library file name
- *
- * @example
- */
-
-exports.getAllRepoTags = (repo) => {
-  var rawContent = execSync(`curl -sSL --compressed ${OFFICIAL_IMAGES_REPO_URL + repo}`).toString()
-  var parsedContent = {}
-
-  if (!rawContent.includes('404: Not Found')) {
-    rawContent = rawContent.split(/(?:\r\n|\r|\n)/g).filter(n => n)
-    for (const line of rawContent) {
-      if (line.startsWith('#')) {
-        // skip comment line
-        continue
-      }
-      // line example: `{"tag":"edge-build-20181207","repoDir":"git://github.com/balena-io-library/base-images@03a5733bc082420e3186d3c89e2805923b0ba40b balena-base-images/aarch64/alpine/edge/build","alias":"edge-build-20181207 edge-build"}`
-      try {
-          var tmpObj = JSON.parse(line)
-          parsedContent[tmpObj.repoDir.split(' ')[1]] = tmpObj.alias.split(' ')
-      }
-      catch (error) {
-        console.log(`Error when reading ${repo} library file!`)
-        continue
-      }
-    }
-    let tags = ''
-    for (const item of Object.keys(parsedContent)) {
-      // something like: `[`jessie`, `latest` (*debian/armv7hf/jessie/Dockerfile*)](https://github.com/balena-io-library/base-images/tree/master/debian/armv7hf/jessie/Dockerfile)`
-      tags += `[${parsedContent[item].join(', ')} (Dockerfile)](https://github.com/balena-io-library/base-images/tree/master/${item}/Dockerfile)\n\n`
-    }
-    return tags
-  }
 }
